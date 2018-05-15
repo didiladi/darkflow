0c345843561ead3bad2788b5981dc34fc7fbe3fb
Dieter Ladenhauf <ladenhauf@protonmail.com>
2018-05-15 17:02:54 +0200

    First version of code ready to perform training

---
+++ models.py
@@ -0,0 +1,133 @@
+
+PATH_TINY_YOLO_VOC_WEIGHTS = '../weights/tiny-yolo-voc.weights'
+
+PATH_CFG_TINY_YOLO_NEW_1 = 'cfg/tiny-yolo-voc-new-1.cfg'
+PATH_CFG_TINY_YOLO_NEW_2 = 'cfg/tiny-yolo-voc-new-2.cfg'
+PATH_CFG_TINY_YOLO_NEW_3 = 'cfg/tiny-yolo-voc-new-3.cfg'
+
+PATH_IMAGES_TRAIN_1 = "../data/images/train-1"
+PATH_IMAGES_TRAIN_2 = "../data/images/train-2"
+PATH_IMAGES_TRAIN_3 = "../data/images/train-3"
+
+PATH_IMAGES_DEV_1 = "../data/images/dev-1"
+PATH_IMAGES_DEV_2 = "../data/images/dev-2"
+PATH_IMAGES_DEV_3 = "../data/images/dev-3"
+
+PATH_IMAGES_TEST_1 = "../data/images/test-1"
+PATH_IMAGES_TEST_2 = "../data/images/test-2"
+PATH_IMAGES_TEST_3 = "../data/images/test-3"
+
+PATH_ANNOTATIONS_TRAIN_1 = "../data/images/train-annotations-1"
+PATH_ANNOTATIONS_TRAIN_2 = "../data/images/train-annotations-2"
+PATH_ANNOTATIONS_TRAIN_3 = "../data/images/train-annotations-3"
+
+PATH_ANNOTATIONS_DEV_1 = "../data/images/dev-annotations-1"
+PATH_ANNOTATIONS_DEV_2 = "../data/images/dev-annotations-2"
+PATH_ANNOTATIONS_DEV_3 = "../data/images/dev-annotations-3"
+
+PATH_ANNOTATIONS_TEST_1 = "../data/images/test-annotations-1"
+PATH_ANNOTATIONS_TEST_2 = "../data/images/test-annotations-2"
+PATH_ANNOTATIONS_TEST_3 = "../data/images/test-annotations-3"
+
+
+class Model(object):
+    """ The abstract base of our models """
+
+    def get_path_for_train_images(self):
+        raise NotImplementedError("Should have implemented this")
+
+    def get_path_for_dev_images(self):
+        raise NotImplementedError("Should have implemented this")
+
+    def get_path_for_test_images(self):
+        raise NotImplementedError("Should have implemented this")
+
+    def get_path_for_train_annotations(self):
+        raise NotImplementedError("Should have implemented this")
+
+    def get_path_for_dev_annotations(self):
+        raise NotImplementedError("Should have implemented this")
+
+    def get_path_for_test_annotations(self):
+        raise NotImplementedError("Should have implemented this")
+
+    def get_config(self):
+        raise NotImplementedError("Should have implemented this")
+
+    def get_weights(self):
+        return PATH_TINY_YOLO_VOC_WEIGHTS
+
+
+class Model1(Model):
+    """ The first model """
+
+    def get_path_for_train_images(self):
+        return PATH_IMAGES_TRAIN_1
+
+    def get_path_for_dev_images(self):
+        return PATH_IMAGES_DEV_1
+
+    def get_path_for_test_images(self):
+        return PATH_IMAGES_TEST_1
+
+    def get_path_for_train_annotations(self):
+        return PATH_ANNOTATIONS_TRAIN_1
+
+    def get_path_for_dev_annotations(self):
+        return PATH_ANNOTATIONS_DEV_1
+
+    def get_path_for_test_annotations(self):
+        return PATH_ANNOTATIONS_TEST_1
+
+    def get_config(self):
+        return PATH_CFG_TINY_YOLO_NEW_1
+
+
+class Model2(Model):
+    """ The second model """
+
+    def get_path_for_train_images(self):
+        return PATH_IMAGES_TRAIN_2
+
+    def get_path_for_dev_images(self):
+        return PATH_IMAGES_DEV_2
+
+    def get_path_for_test_images(self):
+        return PATH_IMAGES_TEST_2
+
+    def get_path_for_train_annotations(self):
+        return PATH_ANNOTATIONS_TRAIN_2
+
+    def get_path_for_dev_annotations(self):
+        return PATH_ANNOTATIONS_DEV_2
+
+    def get_path_for_test_annotations(self):
+        return PATH_ANNOTATIONS_TEST_2
+
+    def get_config(self):
+        return PATH_CFG_TINY_YOLO_NEW_2
+
+
+class Model3(Model):
+    """ The third model """
+
+    def get_path_for_train_images(self):
+        return PATH_IMAGES_TRAIN_3
+
+    def get_path_for_dev_images(self):
+        return PATH_IMAGES_DEV_3
+
+    def get_path_for_test_images(self):
+        return PATH_IMAGES_TEST_3
+
+    def get_path_for_train_annotations(self):
+        return PATH_ANNOTATIONS_TRAIN_3
+
+    def get_path_for_dev_annotations(self):
+        return PATH_ANNOTATIONS_DEV_3
+
+    def get_path_for_test_annotations(self):
+        return PATH_ANNOTATIONS_TEST_3
+
+    def get_config(self):
+        return PATH_CFG_TINY_YOLO_NEW_3
--- transfer-learning.py
+++ transfer-learning.py
@@ -2,6 +2,9 @@
 from darkflow.net.build import TFNet
 import cv2
 import numpy as np
+from enum import Enum
+from models import *
+import subprocess
 
 PATH_CFG_YOLO = 'cfg/yolo.cfg'
 PATH_WEIGHTS_YOLO = '../weights/yolo.weights'
@@ -10,16 +13,43 @@ PATH_CFG_TINY_YOLO = 'cfg/tiny-yolo-voc.cfg'
 PATH_WEIGHTS_TINY_YOLO = '../weights/tiny-yolo-voc.weights'
 
 
+def train_model(model, load_ckpt=False):
+    ''' Trains the given model '''
+
+    cmd = [
+        './flow',
+        '--model', model.get_config(),
+        '--train',
+        '--dataset', model.get_path_for_train_images(),
+        '--annotation', model.get_path_for_train_annotations(),
+        '--load'
+    ]
+    if load_ckpt:
+        cmd.append('-1')
+    else:
+        cmd.append(model.get_weights())
+
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    process.wait()
+    for line in process.stderr:
+        print(line)
+
+
 def predict():
-    options = {"model": PATH_CFG_TINY_YOLO,
-               "load": PATH_WEIGHTS_TINY_YOLO, "threshold": 0.1}
+
+    options = {
+        "model": PATH_CFG_TINY_YOLO,
+        "load": PATH_WEIGHTS_TINY_YOLO,
+        "threshold": 0.1
+    }
 
     tfnet = TFNet(options)
 
     imgcv = cv2.imread("./sample_img/sample_dog.jpg")
     result = tfnet.return_predict(imgcv)
+    tfnet.train
     print(result)
 
 
 if __name__ == '__main__':
-    predict()
+    train_model(Model1())
