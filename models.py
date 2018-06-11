8fcdff03b067a36e774a33d8e5828fa1f0cd5b85
Dieter Ladenhauf <ladenhauf@protonmail.com>
2018-06-11 11:46:54 +0200

    adopting models for eval script

--- models.py
+++ models.py
@@ -1,35 +1,35 @@
 
 PATH_TINY_YOLO_VOC_WEIGHTS = '../weights/tiny-yolo-voc.weights'
 
-PATH_CFG_TINY_YOLO_NEW_1 = 'cfg/tiny-yolo-voc-new-1.cfg'
+PATH_CFG_TINY_YOLO_NEW_1 = 'cfg/tiny-yolo-voc-v2-1.cfg'
 PATH_CFG_TINY_YOLO_NEW_2 = 'cfg/tiny-yolo-voc-new-2.cfg'
 PATH_CFG_TINY_YOLO_NEW_3 = 'cfg/tiny-yolo-voc-new-3.cfg'
 
-PATH_IMAGES_TRAIN_1 = "../data/images/train-1"
+PATH_IMAGES_TRAIN_1 = "../data/images/train-v2-1"
 PATH_IMAGES_TRAIN_2 = "../data/images/train-2"
 PATH_IMAGES_TRAIN_3 = "../data/images/train-3"
 
-PATH_IMAGES_DEV_1 = "../data/images/dev-1"
+PATH_IMAGES_DEV_1 = "../data/images/dev-v2-1"
 PATH_IMAGES_DEV_2 = "../data/images/dev-2"
 PATH_IMAGES_DEV_3 = "../data/images/dev-3"
 
-PATH_IMAGES_TEST_1 = "../data/images/test-1"
+PATH_IMAGES_TEST_1 = "../data/images/test-v2-1"
 PATH_IMAGES_TEST_2 = "../data/images/test-2"
 PATH_IMAGES_TEST_3 = "../data/images/test-3"
 
-PATH_ANNOTATIONS_TRAIN_1 = "../data/images/train-annotations-1"
+PATH_ANNOTATIONS_TRAIN_1 = "../data/images/train-annotations-v2-1"
 PATH_ANNOTATIONS_TRAIN_2 = "../data/images/train-annotations-2"
 PATH_ANNOTATIONS_TRAIN_3 = "../data/images/train-annotations-3"
 
-PATH_ANNOTATIONS_DEV_1 = "../data/images/dev-annotations-1"
+PATH_ANNOTATIONS_DEV_1 = "../data/images/dev-annotations-v2-1"
 PATH_ANNOTATIONS_DEV_2 = "../data/images/dev-annotations-2"
 PATH_ANNOTATIONS_DEV_3 = "../data/images/dev-annotations-3"
 
-PATH_ANNOTATIONS_TEST_1 = "../data/images/test-annotations-1"
+PATH_ANNOTATIONS_TEST_1 = "../data/images/test-annotations-v2-1"
 PATH_ANNOTATIONS_TEST_2 = "../data/images/test-annotations-2"
 PATH_ANNOTATIONS_TEST_3 = "../data/images/test-annotations-3"
 
-PATH_LABELS_1 = "labels.txt"
+PATH_LABELS_1 = "labels-v2-1.txt"
 PATH_LABELS_2 = "labels-2.txt"
 PATH_LABELS_3 = "labels-3.txt"
 
